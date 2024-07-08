09200a0951dbea0740f16b63009d4c8c619b49b8
Emilio Cea <emilio.cea.palma@gmail.com>
2024-07-08 13:33:47 +0200

    add repeated times as param

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(letter string) string {
+func Repeat(letter string, times int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < times; i++ {
 		repeated += letter
 	}
 	return repeated
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,17 +1,25 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 7)
 	}
 }
