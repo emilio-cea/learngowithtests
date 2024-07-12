8169eeabd28c438fc218d899b3039ad18525e964
Emilio Cea <emilio.cea.palma@gmail.com>
2024-07-12 17:15:03 +0200

    configurable sleep

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -18,9 +18,8 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 
 	fmt.Fprint(out, finalWord)
 }
-
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
@@ -35,8 +34,33 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-type DefaultSleeper struct{}
+type SpyCountdownOperations struct {
+	Calls []string
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
