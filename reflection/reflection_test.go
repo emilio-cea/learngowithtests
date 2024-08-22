fb3c659015b3c2f83e11f9eb06ae29c895943d1c
Emilio Cea <emilio.cea.palma@gmail.com>
2024-08-22 11:26:24 +0200

    add maps

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -77,6 +77,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavík"},
 		},
+		{
+			"maps",
+			map[string]string{
+				"Cow":   "Moo",
+				"Sheep": "Baa",
+			},
+			[]string{"Moo", "Baa"},
+		},
 	}
 
 	for _, test := range cases {
@@ -108,6 +116,10 @@ func walk(x interface{}, fn func(input string)) {
 	case reflect.Slice, reflect.Array:
 		numberOfValues = val.Len()
 		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
 	}
 
 	for i := 0; i < numberOfValues; i++ {
