07b91cac9d0714d0cc53af63aab547a31c256549
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-12 17:05:24 +0200

    refactor to svg writer

--- mymaths/clockface/clockface.go
+++ mymaths/clockface/clockface.go
@@ -14,7 +14,11 @@ type Point struct {
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY} //translate
+	return p
 }
 func secondHandPoint(t time.Time) Point {
 	angle := secondsInRadians(t)
