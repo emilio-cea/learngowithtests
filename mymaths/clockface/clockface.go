6dcc1ac7bec2d783db6ba71d559d7be2b0ff0d59
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-12 12:36:18 +0200

    check second hand rough equality because of floats

--- mymaths/clockface/clockface.go
+++ mymaths/clockface/clockface.go
@@ -16,7 +16,13 @@ type Point struct {
 func SecondHand(t time.Time) Point {
 	return Point{150, 60}
 }
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
 
+	return Point{x, y}
+}
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
