ac0a4a3225ede8be84b531cbc75fe4233ee4f4d1
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-13 11:33:04 +0200

    read and create post

--- myfiles/myfiles_test.go
+++ myfiles/myfiles_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/fs"
 	blogposts "myfiles"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
@@ -14,10 +15,22 @@ type StubFailingFS struct {
 func (s StubFailingFS) Open(name string) (fs.File, error) {
 	return nil, errors.New("oh no, i always fail")
 }
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
@@ -25,6 +38,13 @@ func TestNewBlogPosts(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+	assertPost(t, posts[0], blogposts.Post{Title: "Post 1", Description: "Description 1"})
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
