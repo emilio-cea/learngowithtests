d6e5f10f75f5530496cf63af0e7a67c53b1fe358
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-23 18:22:36 +0200

    array with generics

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,11 +1,9 @@
 package main
 
+// Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -16,16 +14,25 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
-func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
 
-	return sums
+	return Reduce(numbers, sumTail, []int{})
+}
+
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
+	}
+	return result
 }
