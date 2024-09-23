a511543ffa734c2639a169ab7cfb062d2207e6a3
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-23 18:34:17 +0200

    balance refactor

--- arrays/arraygenerics.go
+++ arrays/arraygenerics.go
@@ -18,3 +18,30 @@ func BalanceFor(transactions []Transaction, name string) int {
 	}
 	return Reduce(transactions, adjustBalance, 0)
 }
+
+func NewTransaction(from, to Account, sum int) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+type Account struct {
+	Name    string
+	Balance int
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
+}
