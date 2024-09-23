136a24cdae809c0e62d7107e57e30132c5895717
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-23 16:38:36 +0200

    parse template once

--- templating/blogrender.go
+++ templating/blogrender.go
@@ -18,15 +18,34 @@ var (
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
+		return nil, err
+	}
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
-
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
-		return err
-	}
-
 	return nil
 }
+
+// func Render(w io.Writer, p Post) error {
+// 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
