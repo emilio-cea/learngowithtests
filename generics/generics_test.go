f0b15679581b0bc51891e47108c42b4cb7324502
Emilio Cea <emilio.cea.palma@gmail.com>
2024-09-23 18:13:08 +0200

    fix generics

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -15,15 +15,14 @@ func TestAssertFunctions(t *testing.T) {
 
 	// AssertEqual(t, 1, "1") // uncomment to see the error
 }
-
-func AssertEqual[T comparable](t *testing.T, got, want T) {
+func AssertEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
 
-func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+func AssertNotEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got == want {
 		t.Errorf("didn't want %v", got)
@@ -44,7 +43,7 @@ func AssertFalse(t *testing.T, got bool) {
 }
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
+		myStackOfInts := new(Stack[int])
 
 		// check stack is empty
 		AssertTrue(t, myStackOfInts.IsEmpty())
@@ -60,10 +59,17 @@ func TestStack(t *testing.T) {
 		value, _ = myStackOfInts.Pop()
 		AssertEqual(t, value, 123)
 		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		// can get the numbers we put in as numbers, not untyped interface{}
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
 	})
 
 	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(StackOfStrings)
+		myStackOfStrings := new(Stack[string])
 
 		// check stack is empty
 		AssertTrue(t, myStackOfStrings.IsEmpty())
@@ -80,46 +86,25 @@ func TestStack(t *testing.T) {
 		AssertEqual(t, value, "123")
 		AssertTrue(t, myStackOfStrings.IsEmpty())
 	})
+
 }
 
-type StackOfInts struct {
-	values []int
+type Stack[T any] struct {
+	values []T
 }
 
-func (s *StackOfInts) Push(value int) {
+func (s *Stack[T]) Push(value T) {
 	s.values = append(s.values, value)
 }
 
-func (s *StackOfInts) IsEmpty() bool {
+func (s *Stack[T]) IsEmpty() bool {
 	return len(s.values) == 0
 }
 
-func (s *StackOfInts) Pop() (int, bool) {
+func (s *Stack[T]) Pop() (T, bool) {
 	if s.IsEmpty() {
-		return 0, false
-	}
-
-	index := len(s.values) - 1
-	el := s.values[index]
-	s.values = s.values[:index]
-	return el, true
-}
-
-type StackOfStrings struct {
-	values []string
-}
-
-func (s *StackOfStrings) Push(value string) {
-	s.values = append(s.values, value)
-}
-
-func (s *StackOfStrings) IsEmpty() bool {
-	return len(s.values) == 0
-}
-
-func (s *StackOfStrings) Pop() (string, bool) {
-	if s.IsEmpty() {
-		return "", false
+		var zero T
+		return zero, false
 	}
 
 	index := len(s.values) - 1
